7a7b76ed70760a5602cdf58765501ff1b2fcb077
Ashwin7984 <funnyworld6825@gmail.com>
2025-08-16 21:30:44 +0530

    Add files via upload

---
+++ Python Fundamentals/4. Condition Statements/Lab/practical-1.py
@@ -0,0 +1,12 @@
+"""
+Write a Python program to find greater and less than a number using
+if else.
+"""
+
+n1 = int(input("Enter a number1 :- "))
+s=10
+
+if n1 > s:
+    print(f"{n1} Is Greater Than {s}")
+else:
+    print(f"{n1} Is less than {s}")
